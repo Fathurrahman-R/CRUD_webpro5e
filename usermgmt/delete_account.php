8582376e2c864ef9adf34ffae5143c4e7cd577c8
Fathurrahman-R <mr.darksot123@gmail.com>
2025-10-02 15:38:10 +0700

    fix user management

---
+++ usermgmt/delete_account.php
@@ -0,0 +1,16 @@
+
+<?php
+
+include 'connect.php';
+
+$sql = "DELETE FROM users WHERE id=$_GET[id]";
+
+if ($conn->query($sql) === TRUE) {
+//   echo "Record deleted successfully";
+    header('Location: view_all_account.php');
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+?>
