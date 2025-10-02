b86cb918418c47c0e193b6796c7af6a3f37d5e82
Muhammad Fathurrahman <mr.darksot123@gmail.com>
2025-10-02 15:50:04 +0700

    Delete tabel_user directory

--- tabel_user/form_login.php
+++
@@ -1,7 +0,0 @@
-<h2>Login</h2>
-<form action="auth.php" method="post"> 
-    Username: <br><input type="email" name="username"><br>
-    Password: <br><input type="password" name="password"><br>
-    <p>Belum memiliki akun? <a href="form_register.php">Daftar</a></p>
-    <input type="submit" value="Login">
-</form>
